fbd53f5adb95f91b4938e720c8b99804fde13a91
Murtadha <mnisyif@gmail.com>
2026-06-27 12:58:57 +0300

    feature: add commandExit and commandHelp to Pokedexcli

--- repl.go
+++ repl.go
@@ -1,8 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strings"
 )
+
+var commands map[string]cliCommand
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
 
 func cleanInput(text string) []string {
 	var result []string
