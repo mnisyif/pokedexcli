02b1bf6421da43949d950929aae3948730b12b17
Murtadha <mnisyif@gmail.com>
2026-06-27 12:58:22 +0300

    refactor: removed test logic and added support for repl logic

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println(cleanInput("I like this langauge"))
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		cmd, ok := commands[words[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
