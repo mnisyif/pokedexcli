dc5b788fc7ba91b6fab555762925311ba6712ebc
Murtadha <mnisyif@gmail.com>
2026-06-30 14:50:04 +0300

    refactor: integrate caching to pokedexcli

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"time"
+
+	"github.com/mnisyif/pokedexcli/internal/pokeapi"
+	"github.com/mnisyif/pokedexcli/internal/pokecache"
+)
+
 func main() {
-	cfg := &config{}
+	newCache, _ := pokecache.NewCache(5 * time.Second)
+	newClient := &pokeapi.Client{
+		Cache: newCache,
+	}
+	cfg := &config{
+		client: newClient,
+	}
 
 	startREPL(cfg)
 }
